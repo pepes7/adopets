7b0e2b7590091b8ec150ef67b9ed3530e142a042
eddunic <eduardo.bitencourt007@gmail.com>
2019-08-16 18:40:17 -0400

    inclusão de models

--- app/src/main/java/com/example/adopets/config/FirebaseConfig.kt
+++ app/src/main/java/com/example/adopets/config/FirebaseConfig.kt
@@ -1,9 +1,21 @@
 package com.example.adopets.config
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 /**
  * Classe de configuração do Firebase para o App.
  */
-class FirebaseConfig{
+class FirebaseConfig {
 
+    private lateinit var database: DatabaseReference
+
+    fun getFirebaseDatabase(): DatabaseReference {
+        if (database == null) {
+            database = FirebaseDatabase.getInstance().reference
+        }
+
+        return database
+    }
 
 }
